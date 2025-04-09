a4b44a8a77ef258e3fd2da1e10f0efbb3b652016
CarlosCamacho222 <cdcamacho30@gmail.com>
2025-04-08 23:31:03 -0500

    Se crea clase Vehiculo con constructor y los 3 metodos

--- Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Vehiculo.java
+++ Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Vehiculo.java
@@ -1,20 +1,23 @@
 package uniajc.ejercicio2vehiculos;
 
 public class Vehiculo {
-    int velocidadActual, velocidadMaxima;
+
+    int velocidadActual;
+    int velocidadMaxima;
 
     public Vehiculo(int velocidadActual, int velocidadMaxima) {
         this.velocidadActual = velocidadActual;
         this.velocidadMaxima = velocidadMaxima;
     }
 
-    public void imprimir(){
-        
+    public void imprimir() {
     }
-    public int acelerar (int velocidad){
+
+    public int acelerar(int velocidad) {
         return velocidad;
     }
-    public int frenar (int velocidad){
+
+    public int frenar(int velocidad) {
         return velocidad;
     }
 }
