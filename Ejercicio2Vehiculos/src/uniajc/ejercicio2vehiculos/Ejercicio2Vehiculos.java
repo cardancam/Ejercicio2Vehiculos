c5e06e1dc11f51962cdf384c90404e3bf0150cc3
Sebastian Arias <sebastian.arag11@gmail.com>
2025-04-08 23:25:20 -0500

    Se crea clase Main.

--- Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Ejercicio2Vehiculos.java
+++ Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Ejercicio2Vehiculos.java
@@ -4,6 +4,19 @@ package uniajc.ejercicio2vehiculos;
 public class Ejercicio2Vehiculos {
 
     public static void main(String[] args) {
+     // Crear una camioneta (Terrestre) con velocidadActual = 50, velocidadMaxima = 100
+        Terrestre camioneta = new Terrestre(50, 100);
+        System.out.println("🚙 Camioneta:");
+        camioneta.imprimir();
+        System.out.println("Revoluciones: " + camioneta.calcularRevolucionesMotor(10, 5));
+        System.out.println();
+
+        // Crear una moto acuática con velocidadActual = 50, velocidadMaxima = 110
+        Acuatico motoAcuatica = new Acuatico(50, 110);
+        System.out.println("🌊 Moto Acuática:");
+        motoAcuatica.imprimir();
+        int velocidadViento = 90;
+        System.out.println("Velocidad recomendada con viento de " + velocidadViento + " km/h: " + motoAcuatica.recomendarVelocidad(velocidadViento));
+        motoAcuatica.imprimir();
     }
-    
 }
