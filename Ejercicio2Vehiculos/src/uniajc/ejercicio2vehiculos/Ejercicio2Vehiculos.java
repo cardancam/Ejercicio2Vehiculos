6c18308e6d6fda234a7f129eee60dc4ff50a75bb
Sebastian Arias <sebastian.arag11@gmail.com>
2025-04-09 00:08:29 -0500

    Se modifica clase Main.

--- Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Ejercicio2Vehiculos.java
+++ Ejercicio2Vehiculos/src/uniajc/ejercicio2vehiculos/Ejercicio2Vehiculos.java
@@ -5,6 +5,8 @@ public class Ejercicio2Vehiculos {
     public static void main(String[] args) {
      // Crear una camioneta (Terrestre) con velocidadActual = 50, velocidadMaxima = 100
         Terrestre camioneta = new Terrestre(4, "Militar", 50, 100);
+        camioneta.acelerar(25);
+        camioneta.frenar(7);
         System.out.println("Camioneta:");
         camioneta.imprimir();
         System.out.println("Revoluciones: " + camioneta.calcularRevolucionesMotor(10, 5));
@@ -12,6 +14,8 @@ public class Ejercicio2Vehiculos {
 
         // Crear una moto acuática con velocidadActual = 50, velocidadMaxima = 110
         Acuatico motoAcuatica = new Acuatico("Moto Acuatica", 2, 50, 110);
+        motoAcuatica.acelerar(30);
+        motoAcuatica.frenar(15);
         System.out.println("Moto Acuática:");
         motoAcuatica.imprimir();
         int velocidadViento = 90;
